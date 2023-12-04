afc6e055306daf760e35cb0bdfc59ee5c5b393ed
Furkan Keskin <furkankeskin2603@gmail.com>
2023-12-04 16:49:40 +0300

    Initial Commit

---
+++ Evia/Extensions/UIViewControllerExtension.swift
@@ -0,0 +1,30 @@
+//
+//  UIViewControllerExtension.swift
+//  Evia
+//
+//  Created by Furkan on 4.12.2023.
+//
+
+import Foundation
+import UIKit
+import SwiftUI
+extension UIViewController {
+    
+    @available(iOS 13, *)
+    private struct Preview: UIViewControllerRepresentable {
+        let viewController: UIViewController
+        
+        func makeUIViewController(context: Context) -> UIViewController {
+            return viewController
+        }
+        
+        func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+            //
+        }
+    }
+    
+    @available(iOS 13, *)
+    func showPreview() -> some View {
+        Preview(viewController: self)
+    }
+}
---
+++ Evia/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Evia
+//
+//  Created by Furkan on 4.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+import SwiftUI
+#if DEBUG
+
+@available(iOS 13, *)
+struct ViewControllerPreview: PreviewProvider {
+    static var previews: some View {
+        ViewController().showPreview()
+    }
+}
+#endif
